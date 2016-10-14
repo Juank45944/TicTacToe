c2b2c03ac7e277c7601ccf623ae5a2d2d80b3a74
Juank45944 <juan.camilo.cardona.gutierrez@gmail.com>
2016-10-14 13:38:52 -0500

    index2

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -14,25 +14,26 @@ clientSocket.on(evento, (datos) => {
 })
 */
 
+$('#newUser').click(function(){
+  var username = $("[name='username']").val();
+  $('#modal').hide();
+  clientSocket.emit('newUser', {user: username});
+})
 
 
 clientSocket.on('newGame', (datos) => {
   console.log(datos);
 })
 
-class TicTacToe {
-
-  constructor() {
-
-    this.addUser();
-  }
-
-  addUser(){
-    $('#newUser').click(function(){
-      var username = $("[name='username']").val();
-      $('#modal').hide();
-      clientSocket.emit('newUser', {user: username});
-    })
-  }
-  
-}
+// class TicTacToe {
+//
+//   constructor() {
+//
+//     this.addUser();
+//   }
+//
+//   addUser(){
+//
+//   }
+//
+// }
