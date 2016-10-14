d5b1b60c3636aac1535ef511dba56d944f85d365
Juank45944 <juan.camilo.cardona.gutierrez@gmail.com>
2016-10-14 14:40:46 -0500

    front js game

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import $ from 'jquery'
 
-var clientSocket = io()
+//var clientSocket = io()
 
 // Emitir eventos:
 /*
@@ -12,28 +12,76 @@ clientSocket.emit(nombreEvento, { params })
 clientSocket.on(evento, (datos) => {
 
 })
-*/
+// */
+//
+// $('#newUser').click(function(){
+//   var username = $("[name='username']").val();
+//   $('#modal').hide();
+//   clientSocket.emit('newUser', {user: username});
+// })
+//
+//
+// clientSocket.on('newGame', (datos) => {
+//   console.log(datos);
+// })
 
-$('#newUser').click(function(){
-  var username = $("[name='username']").val();
-  $('#modal').hide();
-  clientSocket.emit('newUser', {user: username});
-})
+class TicTacToe {
 
+  constructor() {
+    this.myUser = '';
+    this.players = [];
+    this.clientSocket = io();
+    this.marker = [];
+    this.addUser();
+  }
 
-clientSocket.on('newGame', (datos) => {
-  console.log(datos);
-})
+  addUser(){
+    $('#newUser').click(function(){
+      this.myUser = $("[name='username']").val();
+      $('#modal').hide();
+      clientSocket.emit('newUser', {user: this.myUser});
+    })
+  }
 
-// class TicTacToe {
-//
-//   constructor() {
-//
-//     this.addUser();
-//   }
-//
-//   addUser(){
-//
-//   }
-//
-// }
+  gameStarted(){
+    this.clientSocket.on('newGame', (datos) => {
+      this.players = datos.users;
+      if (this.players[datos.turn]===this.myUser) {
+        this.marker[0] = 'X';
+        this.marker[1] = 'O';
+        miTurno();
+      }else{
+        this.marker[0] = 'O';
+        this.marker[1] = 'X';
+        turnoOponente();
+      }
+    })
+  }
+
+  miTurno(){
+    $(".mi-turno").addClass('active');
+    $('.turno-oponente').removeClass('active');
+    this.addGameClick();
+  }
+  turnoOponente(){
+    $(".turno-oponente").addClass('active');
+    $('.mi-turno').removeClass('active');
+    $('.cuadro').off('click');
+  }
+
+  addGameClick(){
+    $('.cuadro').on('click', function(event){
+      let element = $(event.target).attr('data-number');
+      $(event.target).text(this.marker[0]);
+      this.clientSocket.emit('movement', {target: element, user: this.myUser});
+      this.turnoOponente();
+    })
+  }
+  listenGameClick(){
+    this.clientSocket.on('movement', (datos) => {
+      $(`.cuadro[data-number='${datos.target}']`).text(this.marker[1]);
+      this.miTurno();
+    })
+  }
+
+}
