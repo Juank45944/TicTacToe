da77cc710fe4c3230273cdbd8a15a5a06cdef5d2
emonsalve <edward.monsalve@integ.ro>
2016-10-21 09:45:37 -0500

    Transcribe to Ecma5

--- src/server/index.js
+++ src/server/index.js
@@ -2,25 +2,25 @@
 * Dependencias
 */
 
-import http from 'http'
-import express from 'express'
-import socketio from 'socket.io'
+var http = require('http')
+var express = require('express')
+var socketio = require('socket.io')
 
-const port = 8082
-const app = express()
+var port = 8082
+var app = express()
 
 app.use(express.static('public'))
-const Server = http.createServer(app)
+var Server = http.createServer(app)
 
-const io = socketio(Server)
+var io = socketio(Server)
 
 var currentUsers = []
 var currentRoom = ''
 var firstSocket = ''
 
-io.on('connection', (socket) => {
-    console.log(`New user connected to the game, id: ${socket.id}`)
-    socket.on('newUser', (user) => {
+io.on('connection', function (socket) {
+    console.log('New user connected to the game, id: '+socket.id)
+    socket.on('newUser', function (user) {
         switch (currentUsers.length) {
             case 0:
                 currentUsers.push(user.user)
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
             case 1:
                 currentUsers.push(user.user)
                 currentRoom = currentRoom + user.user
-                let random = Math.floor(Math.random() * currentUsers.length + 1)
+                var random = Math.floor(Math.random() * currentUsers.length + 1)
                 socket.join(currentRoom)
                 firstSocket.join(currentRoom)
                 io.to(currentRoom).emit('newGame', { users: currentUsers, turn: random })
@@ -41,23 +41,25 @@ io.on('connection', (socket) => {
         }
     })
 
-    socket.on('restartGame', (data) => {
-        let usuarios = data.users,
+    socket.on('restartGame', function (data) {
+        var usuarios = data.users,
             random = Math.floor(Math.random() * usuarios.length + 1)
         io.to(currentRoom).emit('newGame', { users: usuarios, turn: random })
     })
 
-    socket.on('movement', (data) => {
+    socket.on('movement', function (data) {
         socket.broadcast.to(currentRoom).emit('movement', data)
     })
 
-    socket.on('message', (message) => {
+    socket.on('message', function (message) {
         socket.broadcast.to(currentRoom).emit('message', message)
     })
-    socket.on('finTurno', () => {
+    socket.on('finTurno', function () {
       socket.emit('finTurno')
       socket.broadcast.to(currentRoom).emit('miTurno')
     })
 })
 
-Server.listen(port, () => console.log(`TitTacToe is ready for play on port ${port}`))
+Server.listen(port, function () {
+    console.log('TitTacToe is ready for play on port: '+port)
+})
